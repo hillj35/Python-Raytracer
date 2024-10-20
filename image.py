87e9321c528a8d0dec05be4f754a90973fafe674
Jordan Hill <jordan.hill@comcast.net>
2024-10-20 00:36:57 -0700

    final scene

--- image.py
+++ image.py
@@ -5,22 +5,47 @@ from TracerTypes.hittable import hittable_list
 from TracerTypes.sphere import sphere
 from TracerTypes.vec3 import vec3 as color, vec3 as vec3
 import TracerTypes.material as material
+from TracerTypes.tracer_util import random_float
 
 def write_image(aspect_ratio: float, width: int, filename: str):
-    # Materials
-    material_ground = material.lambertian(color(0.8, 0.8, 0.0))
-    material_center = material.lambertian(color(0.1, 0.2, 0.5))
-    material_left = material.dielectric(1.50)
-    material_bubble = material.dielectric(1.00 / 1.50)
-    material_right = material.metal(color(0.8, 0.6, 0.2), 0.9)    
-    
     # World
     world = hittable_list()
-    world.add(sphere(vec3(0, -100.5, -1), 100, material_ground))
-    world.add(sphere(vec3(0,0,-1.2), 0.5, material_center))
-    world.add(sphere(vec3(-1.0,0,-1.0), 0.5, material_left))
-    world.add(sphere(vec3(-1.0,0,-1.0), 0.4, material_bubble))
-    world.add(sphere(vec3(1.0,0,-1.0), 0.5, material_right))
+
+    ground_material = material.lambertian(color(0.5, 0.5, 0.5))
+    world.add(sphere(vec3(0, -1000, 0), 1000, ground_material))
+              
+    for a in range(-11, 11):
+        for b in range(-11, 11):
+            choose_mat = random_float()
+            center = vec3(a + 0.9*random_float(), 0.2, b + 0.9 * random_float())
+
+            if ((center - vec3(4, 0.2, 0)).magnitude() > 0.9):
+                if choose_mat < 0.8:
+                    # diffuse
+                    albedo = vec3.random() * vec3.random()
+                    sphere_mat = material.lambertian(albedo)
+                    world.add(sphere(center, 0.2, sphere_mat))
+                elif choose_mat < 0.95:
+                    # metal
+                    albedo = vec3.random(0.5, 1.0)
+                    fuzz = random_float(0, 0.5)
+                    sphere_mat = material.metal(albedo, fuzz)
+                    world.add(sphere(center, 0.2, sphere_mat))
+                else:
+                    # glass
+                    sphere_mat = material.dielectric(1.5)
+                    world.add(sphere(center, 0.2, sphere_mat))
+
+    material1 = material.dielectric(1.5)
+    world.add(sphere(vec3(0, 1, 0), 1.0, material1))
+
+    material2 = material.lambertian(color(0.4, 0.2, 0.1))
+    world.add(sphere(vec3(-4, 1, 0), 1.0, material2))
+
+    material3 = material.metal(color(0.7, 0.6, 0.5), 0.0)
+    world.add(sphere(vec3(4, 1, 0), 1.0, material3))
+
+    
 
     # Camera
     cam = camera()
@@ -29,11 +54,11 @@ def write_image(aspect_ratio: float, width: int, filename: str):
     cam.samples_per_pixel = 10
 
     cam.vfov = 20
-    cam.lookfrom = vec3(-2, 2, 1)
-    cam.lookat = vec3(0, 0, -1)
+    cam.lookfrom = vec3(13, 2, 3)
+    cam.lookat = vec3(0, 0, 0)
     cam.vup = vec3(0, 1, 0)
 
-    cam.defocus_angle = 10
-    cam.focus_dist = 3.4
+    cam.defocus_angle = 0.6
+    cam.focus_dist = 10.0
 
     cam.render(world, filename)
